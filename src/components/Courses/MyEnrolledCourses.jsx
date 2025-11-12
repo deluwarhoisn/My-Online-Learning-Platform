8ffdd186dc7b5188eedfd2eb667d7d7cae7aaaa8
deluwarhoisn <babuhossen301@gmail.com>
2025-11-12 23:05:54 +0600

    update fetch URL in MyCourses component to use production server

--- src/components/Courses/MyEnrolledCourses.jsx
+++ src/components/Courses/MyEnrolledCourses.jsx
@@ -10,7 +10,7 @@ const MyCourses = () => {
   useEffect(() => {
     if (!user?.email) return;
 
-    fetch(`http://localhost:3000/Online`)
+    fetch(`https://online-learning-platfrom-server.vercel.app/Online`)
       .then(res => res.json())
       .then(data => {
         setEnrolledCourses(data);
